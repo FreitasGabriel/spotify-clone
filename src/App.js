68c6b22439708dcfa80e7d6079cb17edf47b4a29
gabriel_fs <gabriel.freitas@bemobi.com.br>
2019-07-22 09:11:42 -0300

    arranging project struture and adding favicon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import GlobalStyle from './styles/global'
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <Fragment>
+        <GlobalStyle />
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    </Fragment>
+)
 
 export default App;
