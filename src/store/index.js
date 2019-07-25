b5f51e782351f44d718effd0c214855a13e0e64a
gabriel_fs <gabriel.freitas@bemobi.com.br>
2019-07-25 09:59:44 -0300

    update configs

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,25 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
-import sagas from './sagas'
-import reducers from './ducks'
+import reducers from './ducks';
+import sagas from './sagas';
 
 const middlewares = [];
 
 const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
-const sagaMiddleware = createSagaMiddleware({sagaMonitor})
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 middlewares.push(sagaMiddleware);
 
-const createAppropriateStore = process.env.NODE_ENV === 'development' ? console.tron.createStore : createStore;
+const composer = process.env.NODE_ENV === 'development'
+  ? compose(
+    applyMiddleware(...middlewares),
+    console.tron.createEnhancer(),
+  )
+  : compose(applyMiddleware(...middlewares));
 
-const store = createAppropriateStore(reducers, compose(applyMiddleware(...middlewares)));
+const store = createStore(reducers, composer);
 
 sagaMiddleware.run(sagas);
 
